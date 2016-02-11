32caa2c2decc28b30fe09881c3fdf75445574ea8
Neviim <neviimdev@gmail.com>
2016-02-11 16:29:02 -0200

    Inicio do codigo do projeto.

--- src/nvmPerceptron.py
+++ src/nvmPerceptron.py
@@ -1,6 +1,30 @@
 # encoding: utf-8
 
-# Ao estudar os fundamentos de redeneural a mais de dois anos, creio que esta na hora de
-# escrever a implementação de uma rede Perceptron, vamos ao codigo.
+#
+# Depos de estar estudando os fundamentos de redeneural a mais de dois anos, creio que esta na hora
+# de escrever minha primeira implementação do zero de uma rede Perceptron, vamos ver o que sai...
+#
 
 __autor__ = "Neviim Jads"
+
+
+class Perceptron:
+
+    def __init__(self):
+        pass
+
+    def treinar(self):
+        pass
+
+    def testar(self):
+        pass
+
+    def degrau(self):
+        pass
+
+    def sinal(self):
+        pass
+
+
+if __name__ == '__main__':
+   pass
