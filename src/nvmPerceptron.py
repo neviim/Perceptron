ac5cb8765e9d56edacfdd5d3e7d842fd03d15312
Neviim <neviimdev@gmail.com>
2016-02-11 16:15:10 -0200

    Inicio do codigo do projeto.

--- src/nvmPerceptron.py
+++ src/nvmPerceptron.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-#
-# Implementar uma rede Perceptron
+# Ao estudar os fundamentos de redeneural a mais de dois anos, creio que esta na hora de
+# escrever a implementação de uma rede Perceptron, vamos ao codigo.
 
 __autor__ = "Neviim Jads"
